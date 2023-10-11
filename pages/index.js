3b58a6b9b3f4771b8a08db42040ff0a754f41d93
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2023-10-11 10:43:16 +0530

    navigation with link component

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
+import Link from 'next/link'
 import React from 'react'
 
-const index = () => {
+const Home = () => {
   return (
     <div>
       <h1>Hello</h1>
+      <Link href="/blog">
+        Blog
+      </Link>
+      <Link href="/product">Product</Link>
     </div>
   )
 }
 
-export default index
+export default Home
--- pages/product/index.js
+++ pages/product/index.js
@@ -1,11 +1,16 @@
+import Link from "next/link";
 import React from "react";
 
-const index = () => {
+const index = ({productId=100}) => {
   return (
     <div>
-      <h1>product 1</h1>
-      <h1>product 2</h1>
-      <h1>product 3</h1>
+      <Link href="/">
+      Home
+      </Link>
+      <h1><Link href="/product/1">product 1</Link></h1>
+      <h1><Link href="/product/2">product 2</Link></h1>
+      <h1><Link href="/product/3" replace>product 3</Link></h1>
+      <h1><Link href={`/product/${productId}`}>product {productId}</Link></h1>
     </div>
   );
 };
