195d910b74fb0febb657e52f50a487d9b4c9c88d
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2023-10-11 10:48:54 +0530

    navigation programatically

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import React from 'react'
 
 const Home = () => {
+  const router = useRouter()
+  const handleClick=()=>{
+    console.log('placing your order');
+    router.push("/product");
+  } 
   return (
     <div>
       <h1>Hello</h1>
@@ -9,6 +15,7 @@ const Home = () => {
         Blog
       </Link>
       <Link href="/product">Product</Link>
+      <button onClick={handleClick}>place order</button>
     </div>
   )
 }
