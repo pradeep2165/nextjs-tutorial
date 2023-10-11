5241fd8ab67c123d345e95bd3e51edc502e6af4f
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2023-10-11 07:14:10 +0530

    dynamic route

---
+++ pages/product/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>
+  <h1>product 1</h1>
+  <h1>product 2</h1>
+  <h1>product 3</h1>
+    </div>
+  )
+}
+
+export default index
---
+++ pages/product/sweater.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const sweater = () => {
+  return (
+    <div>
+      <h1>this is sweater page</h1>
+    </div>
+  )
+}
+
+export default sweater
