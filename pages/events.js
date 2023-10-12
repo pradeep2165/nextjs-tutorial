3e1ec5f102283aeaf4dd8a126ee17ec3cff45657
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2023-10-13 00:50:39 +0530

    shalow routing

--- pages/events.js
+++ pages/events.js
@@ -1,11 +1,14 @@
+import { useRouter } from "next/router";
 import { useState } from "react";
 
 export default function({eventList}){
     const [events, setEvents] = useState(eventList)
+    const router = useRouter()
     const fetchSportEvents=async()=>{
         const response = await fetch('http://localhost:4000/events?category=sports');
         const data = await response.json()
         setEvents(data)
+        router.push('/events?category=sports', undefined, {shallow:true})
     }
     return(
         <>
@@ -25,8 +28,11 @@ export default function({eventList}){
         </>
     )
 }
-export async function getServerSideProps(){
-    const response = await  fetch('http://localhost:4000/events')
+export async function getServerSideProps(context){
+    const {query} = context
+    const {category} = query
+    const queryString = category? 'category=sport': "";
+    const response = await  fetch(`http://localhost:4000/events?${queryString}`)
     const data = await response.json()
     return{
         props:{
