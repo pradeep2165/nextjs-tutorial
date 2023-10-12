4f2f9d5f9741745a7108e843abc4b2b690ab4079
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2023-10-12 11:10:39 +0530

    getStaticPaths fallback true

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -1,4 +1,10 @@
+import { useRouter } from "next/router";
+
 export default function Post({post}){
+    const router = useRouter()
+    if(router.isFallback){
+        return ( <h1>Loading...</h1>)
+    }
     return(
     <><h2>{post.id} {post.title}</h2>
     <p>{post.body}</p>
@@ -30,13 +36,19 @@ export async function getStaticPaths(){
 
         ],
         // paths,
-        fallback:false //404 error if we search http://localhost:3000/posts/4
+        fallback:true //404 error if we search http://localhost:3000/posts/4
     }
 }
 export async function getStaticProps(context){
     const {params} = context
     const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`)
     const data = await response.json()
+
+    if(!data.id){
+        return {
+            notFound:true
+        }
+    }
     return{
         props:{
             post:data
