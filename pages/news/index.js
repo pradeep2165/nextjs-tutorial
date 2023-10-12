4087dfe9747d951bc9a001f51981d350d8b381bf
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2023-10-12 22:03:55 +0530

    ssr rendering integrate

---
+++ pages/news/index.js
@@ -0,0 +1,25 @@
+export default function NewsArticles({articles}){
+    return(
+        <div>
+            <h1>List of News Articles</h1>
+            {articles.map(article =>{
+                return(
+                    <div key={article.id}>
+                        <h2> {article.id} {article.title} | {article.category}</h2>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+
+export async function getServerSideProps(){
+    const response = await fetch('http://localhost:4000/news')
+    const data = await response.json()
+    return {
+        props:{
+            articles: data,
+        }
+    }
+}
