d34b223aacc79f68da6c708c0b17b76af0d5c1c9
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2023-10-13 09:24:39 +0530

    handeling api post request

--- pages/comments/index.js
+++ pages/comments/index.js
@@ -2,18 +2,32 @@ import { useState } from "react"
 
 export default function CommentPage(){
     const [comments, setComments] = useState([])
+    const [comment, setComment] = useState('')
     const  fetchComments = async ()=>{
         const response = await fetch(`/api/comments`);
         const data = await response.json();
         setComments(data);
     }
+    const submitComment=async()=>{
+        const response = await fetch('/api/comments',{
+            method:'POST',
+            body: JSON.stringify({comment}),
+            headers:{
+                'Content-Type':'application/json'
+            }
+        });
+        const data = await response.json();
+        console.log(data);
+    }
     return(
         <>
+        <input type="text"  value={comment} onChange={(e)=>setComment(e.target.value)}/>
+        <button onClick={submitComment}>Submit comment</button>
         <button onClick={fetchComments}>Load comments</button>
         {comments.map((comment)=>{
             return(
                 <div key={comment.id}>
-                    {comment.id}{comment.text}
+                    {comment.id}{" "}{comment.text}
                 </div>
             )
         })}
