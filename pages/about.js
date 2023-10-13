d8b9a0c7635eb9412955ab4e34a97d951f4b9a12
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2023-10-13 14:38:06 +0530

    global styling

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,10 @@
-import React from 'react'
-
+import React from 'react' 
 const about = () => {
   return (
     <div>
       <h1>About page</h1>
+      <h2>hello Pradeep</h2>
+      <button className='btn btn-primary'>button</button>
     </div>
   )
 }
