4aadd2106cf72be85860fcf53efc89bb0f1cdca0
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2023-10-13 12:41:57 +0530

    api and pre-rendering

---
+++ pages/comments/[commentId].js
@@ -0,0 +1,29 @@
+import {comments} from '../../data/comments'
+export default function Comment({ comment }) {
+  return (
+    <div>
+      {comment.id} {comment.text}
+    </div>
+  );
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [{ params: { commentId: "1" } }, { params: { commentId: "2" } }, { params: { commentId: "3" } }],
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context){
+    const {params} = context
+    const {commentId} = params
+    console.log(comments)
+
+    const comment = comments.find((comment)=>comment.id === parseInt(commentId))
+
+    return {
+        props:{
+            comment
+        }
+    }
+}
