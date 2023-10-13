61d024e66b078ae0cfe94ac2d4ec99837e830039
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2023-10-13 11:42:48 +0530

    delete api request

--- pages/comments/index.js
+++ pages/comments/index.js
@@ -19,6 +19,13 @@ export default function CommentPage(){
         const data = await response.json();
         console.log(data);
     }
+    const deleteComment=async (comment_id)=>{
+        const response = await fetch(`api/comments/${comment_id}`,{
+            method:'DELETE',
+        })
+        const data = await response.json();
+        fetchComments()
+    }
     return(
         <>
         <input type="text"  value={comment} onChange={(e)=>setComment(e.target.value)}/>
@@ -28,6 +35,7 @@ export default function CommentPage(){
             return(
                 <div key={comment.id}>
                     {comment.id}{" "}{comment.text}
+                    <button onClick={()=>deleteComment(comment.id)}>Delete</button>
                 </div>
             )
         })}
