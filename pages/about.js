538eb065501fa42480f12475591ba02e2cb73dbf
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2023-10-13 16:36:52 +0530

    sass intigration

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import style from './about.module.css' 
+import style from '../styles/about.module.scss' 
 const about = () => {
   return (
     <div>
-      <h1 className={style.highlight}>About page</h1>
+      <h1 className={style.highlightscss}>About page</h1>
       <h2>hello Pradeep</h2>
       <button className='btn btn-primary'>button</button>
     </div>
---
+++ pages/contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import style from '../styles/contact.module.scss' 
+const Contact = () => {
+  return (
+    <div>
+      <h1 className={style.highlightscss}>Contact page</h1>
+      <h2>hello Pradeep</h2>
+      <button className='btn btn-primary'>button</button>
+    </div>
+  )
+}
+
+export default Contact
