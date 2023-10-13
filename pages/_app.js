38b575b9ed006dbdca4fe15e52a543dc16113fbb
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2023-10-13 19:02:10 +0530

    head component

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,19 @@ import Header from '../components/Header'
 import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../styles/layout.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
+  
   if(Component.getLayout){
     return Component.getLayout(<Component {...pageProps}/>)
   }
-  return <><Header/><Component {...pageProps} /><Footer/></>
+  return <>
+  <Head>
+  <title>next js</title>
+  <meta name="description" content="web development"/>
+</Head>
+  <Header/><Component {...pageProps} /><Footer/></>
 }
 
 export default MyApp
---
+++ pages/blog/[blogId].js
@@ -0,0 +1,21 @@
+import Head from "next/head";
+
+export default function Blog({ title, description }) {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+      </Head>
+      <h1 className="content">Article</h1>
+    </>
+  );
+}
+export async function getServerSideProps() {
+  return {
+    props: {
+      title: "Article Title",
+      description: "Article description",
+    },
+  };
+}
