650f1681d1feb4fdf6398b1ce8b2a328efcb9885
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2023-10-13 18:22:57 +0530

    appLayout configure

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
+import Footer from '../components/Footer'
+import Header from '../components/Header'
 import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import '../styles/layout.css'
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  if(Component.getLayout){
+    return Component.getLayout(<Component {...pageProps}/>)
+  }
+  return <><Header/><Component {...pageProps} /><Footer/></>
 }
 
 export default MyApp
--- pages/about.js
+++ pages/about.js
@@ -1,13 +1,24 @@
-import React from 'react'
-import style from '../styles/about.module.scss' 
-const about = () => {
+import React from "react";
+import style from "../styles/about.module.scss";
+import Footer from "../components/Footer";
+
+const About = () => {
   return (
     <div>
       <h1 className={style.highlightscss}>About page</h1>
       <h2>hello Pradeep</h2>
-      <button className='btn btn-primary'>button</button>
+      <button className="btn btn-primary">button</button>
     </div>
-  )
-}
+  );
+};
 
-export default about
+export default About;
+
+About.getLayout = function PageLayout(page) {
+  return (
+    <>
+      {page}
+      <Footer />
+    </>
+  );
+};
